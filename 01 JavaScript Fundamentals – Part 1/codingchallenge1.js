ad4bb3af45af314ae9a34feea9dc59a8683023a7
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-10 01:55:53 +0530

    coding challenge 1 done

---
+++ 01 JavaScript Fundamentals – Part 1/codingchallenge1.js
@@ -0,0 +1,21 @@
+function details(){
+    
+let mark_M = document.getElementById("mass1").value;
+let mark_H = document.getElementById("height1").value;
+let jhon_M = document.getElementById("mass2").value;
+let jhon_H = document.getElementById("height2").value;
+
+let mark_BMI = BMI(mark_M,mark_H);
+let jhon_BMI = BMI(jhon_M,jhon_H);
+
+let markHigherBMI = (mark_BMI > jhon_BMI) ? "true" : "false";
+alert("Mark's BMI is " + mark_BMI);
+alert("Jhon's BMI is " + jhon_BMI);
+alert(markHigherBMI)
+}
+
+function BMI(mass,height){
+    return mass / height ** 2 ;
+}
+
+
