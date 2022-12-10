5d600db094079f4bec7f469cb1613417a0646f66
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-10 15:23:39 +0530

    added string literals

---
+++ 01 JavaScript Fundamentals – Part 1/string.js
@@ -0,0 +1,18 @@
+
+let name = 'Ishara';
+let age = 30;
+
+//use blackticks
+let my = `I am ${name}`;
+console.log(my);
+
+//multi line strings
+
+console.log("hello \n\
+whats up\n\
+"); //either this way 
+
+console.log(`hello
+whatsup`) //or this way for multiple line stings
+
+
