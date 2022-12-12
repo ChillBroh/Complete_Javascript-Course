e89c60616758b1cba3740f091c28ac19072abd7b
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-12 16:59:19 +0530

    used arrow function

--- 02.JavaScript Fundamentals – Part 2/functions.js
+++ 02.JavaScript Fundamentals – Part 2/functions.js
@@ -1,20 +1,23 @@
-
+                //function declaration
 //can call before declaration
 console.log(age2(1998));
-
 function age2(birthyear){
     return (2023 - birthyear);
 }
 
-
-//save a function body to a variable and this variable will work as function name
-
+                
+                //function expression
 const age1 = function(birthyear){
     return (2023 - birthyear);
 }
-
 //can call only  after function declaration
 console.log(age1(2000))
+
+                //arrow function
+const age3 = birthyear => 2023 - (birthyear);
+//can call only after the expression
+console.log(age3(1999))
+
 
 
 
