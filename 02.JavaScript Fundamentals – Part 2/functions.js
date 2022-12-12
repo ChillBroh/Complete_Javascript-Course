7bd9a6edfff431f991ea592d74d467a56c0c8f9b
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-12 17:09:16 +0530

    arrow function with multiple parameters

--- 02.JavaScript Fundamentals – Part 2/functions.js
+++ 02.JavaScript Fundamentals – Part 2/functions.js
@@ -16,7 +16,14 @@ console.log(age1(2000))
                 //arrow function
 const age3 = birthyear => 2023 - (birthyear);
 //can call only after the expression
-console.log(age3(1999))
+console.log(age3(1999));
+
+//use arrow with multiple parameters
+const age4 = (birthyear,name) => { 
+    const age = (2023 - (birthyear));   
+    return `${name} is ${age} years old`
+};
+console.log(age4(1990,"Ishara"));
 
 
 
