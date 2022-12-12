0daad846450ef3bb5bc9e7490da91cfbdb9bc2d8
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-12 16:32:26 +0530

    calculate age

--- 02.JavaScript Fundamentals – Part 2/strict.js
+++ 02.JavaScript Fundamentals – Part 2/strict.js
@@ -3,6 +3,7 @@
 let hasdrivelicense = false;
 let pasttest = true;
 
-if(pasttest) hasdriveslicense = true;
+if(pasttest) hasdrivelicense = true;
 if(hasdrivelicense) console.log('I can drive');
 
+console.log(2000-1998)
