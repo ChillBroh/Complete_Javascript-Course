6ab73288db7a6fcce8aae762ffc8dedfd04ecf3b
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-14 01:30:34 +0530

    special challenge done

---
+++ 02.JavaScript Fundamentals – Part 2/challenge4special.js
@@ -0,0 +1,11 @@
+
+  const calcAverage = (arr) =>{
+    let total = 0;
+        for(let i =0; i < arr.length; i++){
+            total += arr[i];
+        }
+        return total/arr.length;
+  }
+
+  const array = [20,10,10,30,30];
+  console.log(calcAverage(array));
