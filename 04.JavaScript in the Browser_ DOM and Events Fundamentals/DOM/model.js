dce9482e52b2d9d9d82d365ba5cb74b75b13fb0b
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-15 23:52:07 +0530

    added folder to create pig game

--- 04.JavaScript in the Browser_ DOM and Events Fundamentals/DOM/model.js
+++ 04.JavaScript in the Browser_ DOM and Events Fundamentals/DOM/model.js
@@ -5,14 +5,26 @@ const btnCloseModel = document.querySelector('.close-modal');
 const overley = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 
-for (let i = 0; i < btnShowModel.length; i++) {
-  btnShowModel[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overley.classList.remove('hidden');
-  });
-}
+const openModel = () => {
+  modal.classList.remove('hidden');
+  overley.classList.remove('hidden');
+};
 
-btnCloseModel.addEventListener('click', function () {
+const closeModel = () => {
   modal.classList.add('hidden');
   overley.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModel.length; i++) {
+  btnShowModel[i].addEventListener('click', openModel);
+}
+
+btnCloseModel.addEventListener('click', closeModel);
+overley.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape') {
+    closeModel();
+  }
 });
