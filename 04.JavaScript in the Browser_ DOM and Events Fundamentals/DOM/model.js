d89aae193076e40a7cbb87134f1951cf490a131e
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-15 20:37:35 +0530

    added eventlistners and classlists to display and hide

--- 04.JavaScript in the Browser_ DOM and Events Fundamentals/DOM/model.js
+++ 04.JavaScript in the Browser_ DOM and Events Fundamentals/DOM/model.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const btnShowModel = document.querySelectorAll('.show-modal');
+const btnCloseModel = document.querySelector('.close-modal');
+const overley = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+for (let i = 0; i < btnShowModel.length; i++) {
+  btnShowModel[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overley.classList.remove('hidden');
+  });
+}
+
+btnCloseModel.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overley.classList.add('hidden');
+});
