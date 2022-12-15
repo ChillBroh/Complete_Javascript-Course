49e3eea202bcc330974d013696b96118928ce99e
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-16 01:49:59 +0530

    display images when click roll dice

--- 04.JavaScript in the Browser_ DOM and Events Fundamentals/pig Game/script.js
+++ 04.JavaScript in the Browser_ DOM and Events Fundamentals/pig Game/script.js
@@ -3,7 +3,29 @@
 const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const dice = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const btnNew = document.querySelector(".btn--new");
 
+const current0 = document.querySelector("#current--0");
+const current1 = document.querySelector("#current--1");
 score0.innerHTML = 0;
 score1.innerHTML = 0;
 dice.classList.add("hidden");
+
+const again = () => {
+  score0.innerHTML = 0;
+  score1.innerHTML = 0;
+  current0.innerHTML = 0;
+  current1.innerHTML = 0;
+
+  dice.classList.add("hidden");
+};
+
+btnNew.addEventListener("click", again);
+
+btnRoll.addEventListener("click", function () {
+  const randNum = Math.trunc(Math.random() * 6) + 1;
+  current0.innerHTML = randNum;
+  dice.src = `./images/dice-${randNum}.png`;
+  dice.classList.remove("hidden");
+});
