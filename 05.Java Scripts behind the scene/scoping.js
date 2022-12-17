87cc94406201c4128a6c48bf56dd80d7b1276235
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-17 18:48:03 +0530

    used code runner to run js

--- 05.Java Scripts behind the scene/scoping.js
+++ 05.Java Scripts behind the scene/scoping.js
@@ -1,22 +1,20 @@
 "use strict";
 
-document.getElementById("btn").addEventListener("click", () => {
-  const myName = "ishara";
-  first();
-  function first() {
-    const age = 25;
+const myName = "ishara";
+first();
+function first() {
+  const age = 25;
 
-    if (age >= 25) {
-      const decade = 3;
-      var millenial = true;
-    }
-
-    function second() {
-      const job = "student";
-
-      console.log(`${myName} is a ${age} -old ${job}`);
-    }
-
-    second();
+  if (age >= 25) {
+    const decade = 3;
+    var millenial = true;
   }
-});
+
+  function second() {
+    const job = "student";
+
+    console.log(`${myName} is a ${age} -old ${job}`);
+  }
+
+  second();
+}
