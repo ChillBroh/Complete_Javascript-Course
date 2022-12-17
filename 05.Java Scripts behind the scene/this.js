19420252a7568b0a75e833d309538ff95b7d8b4c
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-12-17 20:43:53 +0530

    initial commit " ""

--- 05.Java Scripts behind the scene/this.js
+++ 05.Java Scripts behind the scene/this.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log(this); //show windows object if not used use strict
-
+let name = "chill";
 const calcAge = function (birthYear) {
   console.log(2037 - birthYear);
   console.log(this); //in strict mode this will show as undefined. not like global function
@@ -14,3 +14,32 @@ const calcAgeArrow = (birthYear) => {
 };
 
 calcAge(1991);
+
+const Ishara = {
+  name: "ishara",
+  year: 1998,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  greet: () => {
+    console.log(`hello ${this.name}`); //ot work undefined. cause arrow don't have this keyword
+  },
+};
+
+Ishara.calcAge();
+
+//copy function from object to object
+const Madusanka = {
+  year: 2000,
+};
+
+Madusanka.calcAge = Ishara.calcAge; //method borrwing
+
+console.log(Madusanka);
+Madusanka.calcAge();
+Ishara.calcAge();
+Ishara.greet(); /*get undefined cause it users window object. all objects are in global scope.they don't have a scope themselves. 
+if we create var name definietly this.name use that var in arrow function*/
+console.log(name);
+console.log(Ishara.name);
