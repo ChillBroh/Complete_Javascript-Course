9ba2cfe9b37b055efae439208cf0d6f8e39d077c
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-03 23:41:22 +0530

    checked short circuit  for AND

--- 06.Data Structures/6.shortcircuitting.js
+++ 06.Data Structures/6.shortcircuitting.js
@@ -1,9 +1,15 @@
 //logical operators can use any data types. return any data type
 //short - circutting (if found truthy first will imediatly return the value)
-
+//OR
 console.log(3 || "Ishara");
 console.log(0 || "Ishara");
 console.log("" || "Ishara");
 console.log(true || 0);
-console.log(undefined || null);
+console.log(undefined || null); //no short circuting
 console.log(undefined || null || "chill");
+
+//AND
+console.log(0 && "Ishara");
+console.log(0 && null);
+console.log("Ishara" && 0);
+console.log(3 && "Ishara"); //no short circuting
