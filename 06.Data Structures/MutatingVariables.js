78b5932df30b310f450de0ffc26e3fb435ecb57f
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-03 17:09:18 +0530

    object destructerd

--- 06.Data Structures/MutatingVariables.js
+++ 06.Data Structures/MutatingVariables.js
@@ -7,3 +7,39 @@ const obj = { a: 23, b: 50, c: 80 };
 ({ a, b } = obj);
 
 console.log(a, b);
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschette", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//nested destructering of object
+const { fri } = restaurant.openingHours;
+console.log(fri);
+
+const {
+  fri: { open, close },
+} = restaurant.openingHours;
+
+console.log(open, close);
