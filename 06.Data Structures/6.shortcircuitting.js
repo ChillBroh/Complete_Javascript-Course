e4d4a2109150bc441ca8a139bc9ae713022e2e42
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-03 23:35:00 +0530

    checked short circuting

---
+++ 06.Data Structures/6.shortcircuitting.js
@@ -0,0 +1,9 @@
+//logical operators can use any data types. return any data type
+//short - circutting (if found truthy first will imediatly return the value)
+
+console.log(3 || "Ishara");
+console.log(0 || "Ishara");
+console.log("" || "Ishara");
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || null || "chill");
