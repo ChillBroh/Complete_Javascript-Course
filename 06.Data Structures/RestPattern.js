f5dfb9a087f96762d36c45be32d4200bb133feaa
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-03 22:55:58 +0530

    rest pattern for objects and arrays

---
+++ 06.Data Structures/RestPattern.js
@@ -0,0 +1,36 @@
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschette", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const [piz, , risot, ...otherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(piz, risot, ...otherFoods);
+
+//objects
+//left side from = operator
+const { sat, ...weekday } = restaurant.openingHours;
+console.log(weekday);
