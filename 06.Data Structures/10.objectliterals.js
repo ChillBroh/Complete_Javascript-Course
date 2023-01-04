1912abadfcd88c430a462e25f8d6fd63da6ff1b3
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-04 12:14:36 +0530

    used es6 object literlas

---
+++ 06.Data Structures/10.objectliterals.js
@@ -0,0 +1,35 @@
+openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschette", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  pasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  // openHours: openingHours - old method
+  openingHours, //with es6
+};
+
+// console.log(restaurant.openHours);
+console.log(restaurant.openingHours);
