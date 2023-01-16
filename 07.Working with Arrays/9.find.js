a46d4592229298648c48c2fa98a986af7f9837de
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-17 00:51:18 +0530

    initial commit

--- 07.Working with Arrays/9.find.js
+++ 07.Working with Arrays/9.find.js
@@ -2,6 +2,7 @@
 //find method use to find the things like what is the first withdraw
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+//only return the first value
 console.log(movements);
 const firstWithdraw = movements.find(mov => mov < 0);
 console.log(`First withdrawed amount is ${Math.abs(firstWithdraw)}`);
