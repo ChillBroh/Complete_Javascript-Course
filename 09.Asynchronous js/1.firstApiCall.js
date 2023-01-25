cdcae3753c6246023d2e9c0567149ad6cb1ab9b8
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-26 00:29:34 +0530

    html for details card done

--- 09.Asynchronous js/1.firstApiCall.js
+++ 09.Asynchronous js/1.firstApiCall.js
@@ -7,14 +7,26 @@ const countriesContainer = document.querySelector('.countries');
 //this is  the most old method xmlhttprequest
 const request = new XMLHttpRequest();
 //open requst
-request.open('GET', 'https://restcountries.com/v3.1/name/Srilanka');
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 //send request
 request.send();
 
 //add event listner when call a function after the ajax call done loading
 request.addEventListener('load', function () {
-  console.log(this.responseText);
   //convert jason file to js object
-  const data = JSON.parse(this.responseText);
+  const [data] = JSON.parse(this.responseText);
   console.log(data);
+  console.log(...data.capital);
+
+  const html = ` 
+  <article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+  </div>
+</article>`;
 });
