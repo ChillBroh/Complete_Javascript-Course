088929856912747fa32c18e5cff044691578da39
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-26 00:06:17 +0530

    added index and css

--- 09.Asynchronous js/1.firstApiCall.js
+++ 09.Asynchronous js/1.firstApiCall.js
@@ -1,1 +1,5 @@
-"use strict";
+'use strict';
+
+//this is  the most old method xmlhttprequest
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/Srilanka');
