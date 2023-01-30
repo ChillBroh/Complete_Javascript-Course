de6c7023d13e6ef7d935a8374c108e0424b69d80
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-30 16:01:52 +0530

    added time'

--- 09.Asynchronous js/7.build a promise.js
+++ 09.Asynchronous js/7.build a promise.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const lottary = new Promise((resolve, reject) => {
-  let num = Math.trunc(Math.random() * 10);
-  if (num >= 5) {
-    console.log(num);
-    resolve('You Won!');
-  } else {
-    console.log(num);
-    reject('Lost Money!');
-  }
+  console.log('lottary Draw is hapenning...');
+  setTimeout(function () {
+    //this will set time. timer is move to call back section while promise in micro task
+    let num = Math.trunc(Math.random() * 10);
+    if (num >= 5) {
+      console.log(num);
+      resolve('You Won!');
+    } else {
+      console.log(num);
+      reject('Lost Money!');
+    }
+  }, 2000);
 });
 
 lottary.then((res) => console.log(res)).catch((err) => console.log(err));
