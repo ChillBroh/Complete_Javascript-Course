b7d069e72a3b2cb691a8bc405d7f69cb93e505bf
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-31 13:21:26 +0530

    initial commit

--- 09.Asynchronous js/8.async await.js
+++ 09.Asynchronous js/8.async await.js
@@ -2,11 +2,14 @@
 
 const btn = document.querySelector('.btn-country');
 
-btn.addEventListener('click', function () {
-  const whereAmI = async function (country) {
-    fetch(`https://restcountries.com/v2/name/${country}`).then((res) =>
-      console.log(res)
-    );
+const mycountry = () => {
+  const whereAmI = function (country) {
+    fetch(`https://restcountries.com/v2/name/${country}`)
+      .then((res) => {
+        console.log(res);
+        res.json();
+      })
+      .then((data) => console.log(data));
   };
   console.log('First');
   console.log('second');
@@ -19,10 +22,15 @@ btn.addEventListener('click', function () {
     const whereAmI2 = async function (country) {
       const ans = await fetch(`https://restcountries.com/v2/name/${country}`);
       console.log(ans);
+      const data = await ans.json(); //no need of then method when we use await
+      console.log(data);
     };
     console.log('First');
     console.log('second');
     whereAmI2('sri lanka');
     console.log('third');
   }, 2000);
+};
+btn.addEventListener('click', () => {
+  mycountry('srilanka');
 });
